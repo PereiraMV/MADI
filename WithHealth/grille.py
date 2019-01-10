37ab526d1eebf60f39486ca279587852adb16861
Julien Andres <julienandres06@gmail.com>
2019-01-10 12:47:42 +0100

    generation + some affichage

--- WithHealth/grille.py
+++ WithHealth/grille.py
@@ -5,9 +5,10 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import random
 
 import time
-def timer(f):#décorateur pour timer du temps que prend le picross à s'executer
+def timer(f):
     def wrapper(*args):
         t0=time.time()
         res=f(*args)
@@ -80,7 +81,7 @@ class grille:
 		return listState,nb
 
 
-	def affichage(self,posPlayer):
+	def affichage2(self,posPlayer):
 
 		for i in range (self.size[0]):
 			for j in range (self.size[1]):
@@ -98,7 +99,7 @@ class grille:
 					
 		return 
 
-	def affichage2(self,posPlayer):
+	def affichage(self,posPlayer,dual=False):
 		change={'B':0,'W':1,'T':2,'E':3,'K':9,'R':5,'C':6,'S':7,'M':8,'P':4,'O':10}
 		toprint=['T','E','R','C','K','S','M','P','O']
 		m=self.tab[:][:]
@@ -108,6 +109,8 @@ class grille:
 		m=new_m
 				
 		fig, ax = plt.subplots()
+		if dual:
+			fig,ax=plt.subplot(1,2,1)
 		m[posPlayer[0]][posPlayer[1]]=11
 		cmap=mpl.colors.ListedColormap(['w', 'k','w'])
 		bounds = np.array([0, 0.1, 1.1,2.1])
@@ -125,13 +128,123 @@ class grille:
 		
 		plt.imshow(m,cmap=cmap,norm=norm,interpolation="none")
 		plt.show()
+		return fig,ax
 		
-	
+	def generate_solvable(self,nb_line,nb_col,pw,po,pc,max_key,max_sword,max_portal,nb_try=30):
+		if nb_line<4 or nb_col<4:
+			print "Dimentions trop petites"
+			return False
+		self.generate(nb_line,nb_col,pw,po,pc,max_key,max_sword,max_portal)
+		sucess=self.is_solvable()
+		while (not(sucess) and nb_try>0):
+			self.generate(nb_line,nb_col,pw,po,pc,max_key,max_sword,max_portal)
+			sucess=self.is_solvable()
+			nb_try-=1
+			print nb_try
+		if nb_try<=0:
+			print "Fail generating"
+			return False
+		else:
+			print "Generation succeed"
+			return True
+			
+		
+		return
+	def generate(self, nb_line, nb_col, pw, po, pc, max_key, max_sword, max_portal):
+		#nbline,nbcol,propotion_wall,proportion_obstacles ,proportion_crack,nombre_key,nombre_max_sword,nombre_max_portal
+	#Obstacle=Ennemy or Traps "E" or "R"
+		self.size = (nb_line,nb_col)
+		self.tab = []
+		for i in range(nb_line):
+			self.tab.append(['B'] * nb_col)
+		for i in range(nb_line):
+			for j in range(nb_col):
+				if random.uniform(0, 1) < pw:
+					self.tab[i][j] = 'W'
+				elif random.uniform(0, 1) < po:
+					#ennemy
+					if random.randint(0, 1) == 0:
+						self.tab[i][j] = 'E'
+					else:
+						self.tab[i][j] = 'R'
+				elif random.uniform(0, 1) < pc:
+					self.tab[i][j] = 'C'
+		#sword
+		for key in range(max_sword):
+			self.tab[random.randint(0, nb_line-1)][random.randint(0, nb_col-1)] = 'S'
+			
+		#portals
+		for key in range(max_portal):
+			self.tab[random.randint(0, nb_line-1)][random.randint(0, nb_col-1)] = 'P'
+		
+		#keys
+		for key in range(max_key):
+			self.tab[random.randint(0, nb_line-1)][random.randint(0, nb_col-1)] = 'K'
+		#tresor
+		self.tab[0][0] = 'T'
+		#starting point
+		self.tab[nb_line - 1][nb_col - 1] = 'O'
 
+
+	def export(self,filename="exemple.txt"):
+		with open(filename,"w") as f:
+			f.write(str(self.size[0])+" "+str(self.size[1])+"\n")
+			for i in range(len(self.tab)):
+				string=""
+				for j in range(len(self.tab[i])):
+					string+=self.tab[i][j]
+					if j!=len(self.tab[i])-1:
+						string+=" "
+				f.write(string+"\n")
+		return
+		
+	def is_solvable(self):
+		if self.tab:
+			if not self.has_key():
+				return False
+			if not self.way_possible("K"):
+				return False
+			if not self.way_possible("T"):
+				return False
+		for i in range(self.size[0]):
+			for j in range(self.size[1]):
+				if not(self.way_possible('Z',i,j)):
+					return False
+		return True
+
+	def has_key(self):
+		for row in self.tab:
+			for cell in row:
+				if cell == "K":
+					return True
+		return False
+		
+	def way_possible(self,objective,indice_i=-1,indice_j=-1):
+		Q = []
+		visited = []
+		Q.append([self.size[0] - 1, self.size[1] - 1])
+		visited.append([self.size[0] - 1, self.size[1] - 1])  # start is visited
+		while len(Q) > 0:
+			node = Q.pop()
+			neighbours = [[node[0] - 1, node[1]], [node[0] + 1, node[1]], [node[0], node[1] - 1],[node[0], node[1] + 1]]
+			for n in neighbours:
+				if 0 <= n[0] < self.size[0] and 0 <= n[1] < self.size[1]:
+					if self.tab[n[0]][n[1]] == objective or (n[0],n[1])==(indice_i,indice_j):
+						return True
+					else:
+						if self.tab[n[0]][n[1]] != "W" and self.tab[n[0]][n[1]] != "C":
+							if n not in visited:
+								Q.append(n)
+								visited.append(n)
+		return False
+		
 
 if __name__ == "__main__":
     
 	grilleA=grille("exemple2.txt")
 	#print(grilleA.checkCell((0,0)))
 #	print(grilleA.size,grilleA.tab)
-	grilleA.affichage2((0,0))
+#	grilleA.affichage((0,0))
+	grilleA.generate_solvable(16,16,.3,.2,.1,1,1,3,nb_try=500)
+#	grilleA.is_solvable()
+	grilleA.affichage((0,0))
--- WithHealth/methods.py
+++ WithHealth/methods.py
@@ -17,8 +17,9 @@ def timer(f):#decorateur pour timer du temps que prend le picross a s executer
         return res
     return wrapper
 
-def iteration_value(pdm,gam):
+def iteration_value(pdm,gam,eps=0.000001):
 	cpt=0
+	
 	for key,state in pdm.listState.items():
 		state.reset()
 	while True:
@@ -41,13 +42,15 @@ def iteration_value(pdm,gam):
 			state.optimal=np.argmax(Q)
 			state.vnext=Q[state.optimal]
 		allEq=True
+		somme=[]
 		for key,state in pdm.listState.items():
+			somme.append(abs(state.vnext-state.vpast))
 			if not(state.compare()):
 				allEq=False
 			state.update()
 		cpt+=1	
-		print(cpt)
-		if allEq:
+		print cpt,np.max(somme)
+		if allEq or np.max(somme)<eps:
 			break
 			
 #	print("compteur "+str(cpt))
@@ -59,7 +62,8 @@ def iteration_value(pdm,gam):
 #				else:
 #					print 4,
 #			print
-	affichage_of_play(pdm,((0,0),0,1,1,1,0))
+#	affichage_of_play(pdm,((0,0),0,1,1,1,0))
+	affichage_policy(pdm)
 
 	return
 @timer
@@ -127,20 +131,26 @@ def optimal_Pl(pdm,gam):
 	
 	
 	####display
-	for i in range(pdm.grille.size[0]):
-		for j in range(pdm.grille.size[1]):
-			if pdm.grille.tab[i][j]!='M' and pdm.grille.tab[i][j]!='P' and pdm.grille.tab[i][j]!='W' :
-				#position sword key tresor type
-				print pdm.listState[((i,j),0,1,0,0,0)].optimal,
-			else:
-				print 4,
-		print
+#	for i in range(pdm.grille.size[0]):
+#		for j in range(pdm.grille.size[1]):
+#			if pdm.grille.tab[i][j]!='M' and pdm.grille.tab[i][j]!='P' and pdm.grille.tab[i][j]!='W' :
+#				#position sword key tresor type
+#				print pdm.listState[((i,j),0,1,1,0,0)].optimal,
+#			else:
+#				print 4,
+#		print
+#	affichage_of_play(pdm,((0,0),0,1,1,1,0))
+	affichage_policy(pdm)
 	return 
 
-def affichage_of_play(pdm,key):#key= cle de listState
+def affichage_of_play(pdm,key,dual=False,ax=None,fig=None):#key= cle de listState
 #	print pdm.listState
+	
 	toprint={0:"^",1:"v",2:"<",3:">"}
+	toprint2=['g','r','m','y']
 	fig, ax = plt.subplots()
+	if dual:
+		plt.subplot(1,2,2)
 	posPlayer,sword,key,health,tresor,typee=key
 	for i in range(pdm.grille.size[0]):
 		for j in range(pdm.grille.size[1]):
@@ -149,8 +159,9 @@ def affichage_of_play(pdm,key):#key= cle de listState
 			if pdm.grille.tab[i][j]!='M' and pdm.grille.tab[i][j]!='P' and pdm.grille.tab[i][j]!='W' :
 				#position sword key tresor type
 				az=pdm.listState[((i,j),sword,key,health,tresor,typee)].optimal
-				ax.scatter(j,pdm.grille.size[0]-i,marker=toprint[az],s=200)
+				ax.scatter(j,pdm.grille.size[0]-i,marker=toprint[az],s=200,color=toprint2[az])
 				print pdm.listState[((i,j),sword,key,health,tresor,typee)].optimal,
+
 			elif pdm.grille.tab[i][j]=='M':
 				ax.scatter(j,pdm.grille.size[0]-i,marker="_",s=400,color="k")
 			elif pdm.grille.tab[i][j]=='P':
@@ -160,8 +171,80 @@ def affichage_of_play(pdm,key):#key= cle de listState
 				print 4,
 		print
 	return
-    
-    
+	
+def affichage_policy(pdm):
+	toplay={0:"z",1:"s",2:"q",3:"d"}
+	statePlayer=pdm.listState[((pdm.grille.size[0]-1,pdm.grille.size[1]-1),0,0,4,0,0)]
+	
+	print "player key "+str(statePlayer.key)+"player health "+str(statePlayer.health)+" player tresor "+str(statePlayer.tresor)+" player sword "+str(statePlayer.sword) 
+	while (True):
+		pdm.grille.affichage(pdm.playerPos)
+#		affichage_of_play(pdm,((0,0),statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,statePlayer.type))
+	
+		if statePlayer.type==0:
+			pc=-1
+			
+			while(pc==-1):
+				
+				playerMove=raw_input("press Enter to continue")
+				playerMove=pdm.listState[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,statePlayer.type)].optimal
+				playerMove=toplay[playerMove]
+				print playerMove
+				if (playerMove=='z'):
+					if pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,0)][0]!=None:
+						pc=0
+				if (playerMove=='s'):
+					if pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,0)][1]!=None:
+						pc=1
+				if (playerMove=='q'):
+					if pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,0)][2]!=None:
+						pc=2
+				if (playerMove=='d'):
+					if pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,0)][3]!=None:
+						pc=3
+
+			statePlayer=pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,0)][pc].listState[0]
+			pdm.playerPos=statePlayer.position
+			
+		if statePlayer.type==1:
+		
+
+
+
+			transPlayer=pdm.listTrans[(statePlayer.position,statePlayer.sword,statePlayer.key,statePlayer.health,statePlayer.tresor,1)][0]
+			proba=transPlayer.listProba
+			states=transPlayer.listState
+			nb=random.random()
+			print "nb rand= "+str(nb)+" len proba "+str(len(proba))
+			print proba
+			sumi=0
+			indice=-1
+#				list_indice=[i for i in range(len(proba))]
+			for i in range(len(proba)):
+				sumi=sumi+proba[i]
+				if nb<=sumi:
+					indice=i
+					break
+#				indice=random.choice(list_indice,proba)
+			print(indice)
+			statePlayer=states[indice]
+			pdm.playerPos=statePlayer.position
+			
+		if statePlayer.type==2:
+			
+			if statePlayer.stateType=="fail":
+				print "you failed"
+				break
+			if statePlayer.stateType=="sucess":
+				print "you win"
+				break
+		print "player key "+str(statePlayer.key)+"player health"+str(statePlayer.health)+" player tresor "+str(statePlayer.tresor)+" player sword "+str(statePlayer.sword) 
+			
+
+
+					
+
+	return
 def next_state(pdm,indice,stateA):
 	
 	transPlayer=pdm.listTrans[(stateA.position,stateA.sword,stateA.key,stateA.health,stateA.tresor,stateA.type)][indice]
@@ -255,8 +338,8 @@ def Qlearning(pdm,gam,taux,timer):
 grilleA=gr.grille("exemple2.txt")
 #grilleA.affichage((0,0))
 pdm=PDM(grilleA)
-iteration_value(pdm,0.6)
-#optimal_Pl(pdm,0.99)
+#iteration_value(pdm,0.99)
+optimal_Pl(pdm,0.99)
 #Qlearning(pdm,0.99,lambda x:1.0/x,10000)
 
 
